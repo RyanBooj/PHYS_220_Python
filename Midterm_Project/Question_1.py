dff3475406ee3f890240c011376551471a841d46
RyanBooj <ryanb3@umbc.edu>
2020-04-02 11:59:13 -0400

    Added explanation to why s_down is more accurate than s_up. Finished with Question_1.py

--- Midterm_Project/Question_1.py
+++ Midterm_Project/Question_1.py
@@ -31,15 +31,19 @@ def s_down(N):
 
     return result
 
+
 # Function to get the values that need to be graphed on the y-axis
 def get_y_vals(N):
-    return (s_up(N) - s_down(N)) / (abs(s_up(N)) + abs(s_down(N)))
+    up = s_up(N)
+    down = s_down(N)
+    return (up - down) / (abs(up) + abs(down))
+
 
 # function to plot n terms
 def plot_n_vals(n):
     y_vals = []
     x_vals = []
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         x_vals.append(i)
         y_vals.append(get_y_vals(i))
 
@@ -53,4 +57,24 @@ def main():
     plot_n_vals(100)
     plot_n_vals(1000)
 
+    # may take a couple of seconds to compute
+    plot_n_vals(10000)
+
+
 main()
+
+# The graphs show the difference between the accuracy of the calculations. When
+# the program is run with smaller values of N, the difference is small, shown by
+# only a single spike in the function. But when larger values of N are given,
+# the spike steadily increase in magnitude. This can be explained by the downward
+# sum being more precise than the upward sum. This is because of the way that Python
+# rounds floating point numbers. More specifically, Python has a limited number
+# of significant digits and when the sum reaches a certain point, the floating
+# point number representing the sum needs to store one more digit than it is able to.
+# This causes Python to round the number so it can be stored. When computing s_down
+# this rounding happens at a different operation than when computing s_up. This results
+# in s_down being able to store most of the smaller sums (since it starts summing from
+# larger values of N first) to a greater degree of precision. Therefore, since s_up loses
+# this extra degree of precision quickly since its sum will be larger for most of the
+# calculation, s_down has a small advantage in precision when dealing with large values
+# of N.
