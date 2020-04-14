406eced04811db80aa5d81118985799934cc0a37
RyanBooj <ryanb3@umbc.edu>
2020-04-14 13:56:27 -0400

    Finished Sensors 2-4 Made graphs save instead of display after each run

--- Final Project/Plotting_Data.py
+++ Final Project/Plotting_Data.py
@@ -54,22 +54,68 @@ def main():
     init = np.ones(4)
 
     # Robust least squares optimization
-    residuals_lsq = least_squares(residuals_fun, init, loss='soft_l1', f_scale=0.5, args=(ADC1, distance))
+    residuals_lsq1 = least_squares(residuals_fun, init, loss='soft_l1', f_scale=0.5, args=(ADC1, distance))
 
-    # plot result
+    # plot results
     plt.figure('Sensor 1')
     ADC1_max = int(np.amax(ADC1))
     ADC1_min = int(np.amin(ADC1))
     plt_sens1_x = np.linspace(ADC1_min, ADC1_max, 10000)
-    plt_sens1_y = generate_data(plt_sens1_x, *residuals_lsq.x)
+    plt_sens1_y = generate_data(plt_sens1_x, *residuals_lsq1.x)
     plt.plot(plt_sens1_x, plt_sens1_y, label='ADC Robust Least Squares Estimation')
     plt.plot(ADC1, distance, 'o', label='Measured Data')
     plt.xlabel('ADC Voltage Values')
     plt.ylabel('Distance From Sensor to Object (CM)')
     plt.legend(loc='upper right')
+    plt.savefig("Sensor1.png")
 
-    plt.show()
+    # Repeat for other sensors
 
+    residuals_lsq2 = least_squares(residuals_fun, init, loss='soft_l1', f_scale=0.5, args=(ADC2, distance))
+
+    plt.figure('Sensor 2')
+    ADC2_max = int(np.amax(ADC2))
+    ADC2_min = int(np.amin(ADC2))
+    plt_sens2_x = np.linspace(ADC2_min, ADC2_max, 10000)
+    plt_sens2_y = generate_data(plt_sens2_x, *residuals_lsq2.x)
+    plt.plot(plt_sens2_x, plt_sens2_y, label='ADC Robust Least Squares Estimation')
+    plt.plot(ADC2, distance, 'o', label='Measured Data')
+    plt.xlabel('ADC Voltage Values')
+    plt.ylabel('Distance From Sensor to Object (CM)')
+    plt.legend(loc='upper right')
+    plt.savefig("Sensor2.png")
+
+    # For second graphs where the length of the ADC data is shorter, only use the corresponding
+    # values from distance (first 22 values)
+    residuals_lsq3 = least_squares(residuals_fun, init, loss='soft_l1', f_scale=0.5, args=(ADC3, distance[:21]))
+
+    plt.figure('Sensor 3')
+    ADC3_max = int(np.amax(ADC3))
+    ADC3_min = int(np.amin(ADC3))
+    plt_sens3_x = np.linspace(ADC3_min, ADC3_max, 10000)
+    plt_sens3_y = generate_data(plt_sens3_x, *residuals_lsq3.x)
+    plt.plot(plt_sens3_x, plt_sens3_y, label='ADC Robust Least Squares Estimation')
+    plt.plot(ADC3, distance[:21], 'o', label='Measured Data')
+    plt.xlabel('ADC Voltage Values')
+    plt.ylabel('Distance From Sensor to Object (CM)')
+    plt.legend(loc='upper right')
+    plt.savefig("Sensor3.png")
+
+    residuals_lsq4 = least_squares(residuals_fun, init, loss='soft_l1', f_scale=0.5, args=(ADC4, distance[:21]))
+
+    plt.figure('Sensor 4')
+    ADC4_max = int(np.amax(ADC4))
+    ADC4_min = int(np.amin(ADC4))
+    plt_sens4_x = np.linspace(ADC4_min, ADC4_max, 10000)
+    plt_sens4_y = generate_data(plt_sens4_x, *residuals_lsq4.x)
+    plt.plot(plt_sens4_x, plt_sens4_y, label='ADC Robust Least Squares Estimation')
+    plt.plot(ADC4, distance[:21], 'o', label='Measured Data')
+    plt.xlabel('ADC Voltage Values')
+    plt.ylabel('Distance From Sensor to Object (CM)')
+    plt.legend(loc='upper right')
+    plt.savefig("Sensor4.png")
+
+    # plt.show()
 
 main()
 
