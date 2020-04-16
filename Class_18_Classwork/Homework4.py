1104dfdea2e7223c67b8cb4abaf3656b3625aa80
RyanBooj <ryanb3@umbc.edu>
2020-04-16 13:09:48 -0400

    Changed answer to include accuracy increase from choosing a shape with a larger radius.

--- Class_18_Classwork/Homework4.py
+++ Class_18_Classwork/Homework4.py
@@ -37,11 +37,11 @@ def monte_carlo(N, radius=1):
 
 # Function to plot the estimation of the value of pi using N from 10-1000
 # and a semicircle with a radius of 4
-def plot_estimation():
+def plot_estimation(N=1000, radius=4):
     # define radius of semicircle and bounding area
-    radius = 10
+
     bound_area = (radius*2)*radius
-    x = np.arange(10, 10010, 10)
+    x = np.arange(10, N+10, 10)
     x = x.astype(int)
     y = np.array([])
     for i in range(np.size(x)):
@@ -81,7 +81,10 @@ def main():
     # # radius of this semicircle is 2
     # pi_est = (area / 2)
 
-    plot_estimation()
+    plot_estimation(10000)
+    # By increasing the area of the shape that I measured, the accuracy of the
+    # estimation increased significantly
+    # plot_estimation(10000, 100)
 
 
 main()
@@ -96,4 +99,8 @@ main()
 # estimation is not only a function of the amount of data points used, but is also based on the
 # values of the random numbers that are generated. If the random numbers that are generated tend
 # more towards values that are below the function curve, then the value of pi will be estimated
-# too large and visa versa.
+# too large and visa versa. One way that I found to reduce the difference between the  estimated
+# value and the actual value was to increase the radius of the semi-circle that I was estimating.
+# Increasing the area from 4 to 100 significantly reduced the difference between my estimation and
+# the actual value. This could be due to the random number generator favoring either large numbers
+# or small numbers, and by increasing the radius, all the numbers became large, increasing the accuracy.
